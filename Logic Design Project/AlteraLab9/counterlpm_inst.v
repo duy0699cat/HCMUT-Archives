2fa757f9796e85a632dfa28619a2a3e33accc215
duy0699cat <31996950+duy0699cat@users.noreply.github.com>
2021-05-26 18:54:59 +0700

    add project

---
+++ Logic Design Project/AlteraLab9/counterlpm_inst.v
@@ -0,0 +1,9 @@
+counterlpm	counterlpm_inst (
+	.clk_en ( clk_en_sig ),
+	.clock ( clock_sig ),
+	.cnt_en ( cnt_en_sig ),
+	.data ( data_sig ),
+	.sclr ( sclr_sig ),
+	.sload ( sload_sig ),
+	.q ( q_sig )
+	);
---
+++ Logic Design Project/AlteraLab9/romlpm_inst.v
@@ -0,0 +1,5 @@
+romlpm	romlpm_inst (
+	.address ( address_sig ),
+	.clock ( clock_sig ),
+	.q ( q_sig )
+	);
