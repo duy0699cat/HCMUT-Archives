872b1ebc4307a98e41fab0e6e9e85ac580889807
duy0699cat <duy0699cat@gmail.com>
2021-10-12 23:15:27 +0700

    auto update?

--- IoT Application Development/Lab/w1/PythonProject1/main.py
+++ IoT Application Development/Lab/w1/PythonProject1/main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import random
+import time
+import  sys
+from  Adafruit_IO import  MQTTClient
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+AIO_FEED_ID = "temp"
+AIO_USERNAME = "duy_ngotu"
+AIO_KEY = "aio_OvuE73LvHaG91YUEO8Lf790ewgUr"
 
+def  connected(client):
+    print("Ket noi thanh cong...")
+    client.subscribe(AIO_FEED_ID)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def  subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong...")
 
+def  disconnected(client):
+    print("Ngat ket noi...")
+    sys.exit (1)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def  message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+
+while True:
+    value = random.randint(0, 100)
+    print("Cap nhat:", value)
+    client.publish("bbc-temp", value)
+    time.sleep(20)
